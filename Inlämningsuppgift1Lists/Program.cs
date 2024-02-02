6ab7f074952dfc5306ebca92a15a8278f7dd0883
DommSite <johan.johansson06@live.se>
2024-02-02 12:22:33 +0100

    Add project files.

---
+++ Inlämningsuppgift1Lists/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+
+namespace listinlämning_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string svar = "123";
+            List<string> namn = new List<string>();
+            while (svar != "")
+            {
+                Console.WriteLine("Skriv in ett namn");
+                svar = Console.ReadLine();
+                if (svar != "")
+                {
+                    namn.Add(svar);
+                }
+            }
+            namn.Sort();
+            namn.Reverse();
+            foreach(string a in namn)
+            {
+                Console.WriteLine(a);
+            }
+        }
+    }
+}
